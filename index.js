56458a09e36043705a78206d5d5ee90b79be63ff
Miles Thomas <milesman2013@gmail.com>
2022-10-18 15:59:48 +1100

    prompt cleanup

---
+++ index.js
@@ -0,0 +1,9 @@
+// Allows inquirer to be used on the page
+const inquirer = require('inquirer')
+
+// Allows utilisation of the pages within the lib folder
+const employee = require('./lib/employee')
+const manager = require('./lib/manager')
+const engineer = require('./lib/engineer')
+const intern = require('./lib/intern')
+
