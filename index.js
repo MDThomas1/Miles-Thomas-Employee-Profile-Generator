b04e41343c534e14e28d8ae4887341f833b4f65a
Miles Thomas <milesman2013@gmail.com>
2022-10-19 19:32:47 +1100

    more small fixes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 // Allows utilisation of the pages within the lib folder
-const Employee = require('./lib/employee')
-const Manager = require('./lib/manager')
-const Engineer = require('./lib/engineer')
-const Intern = require('./lib/intern')
+const Employee = require('./lib/employee.js')
+const Manager = require('./lib/manager.js')
+const Engineer = require('./lib/engineer.js')
+const Intern = require('./lib/intern.js')
 
 function companyCreation() {
     inquirer
