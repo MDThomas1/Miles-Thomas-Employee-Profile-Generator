92342e60357e513953bd77930106284296237670
Miles Thomas <milesman2013@gmail.com>
2022-10-19 15:51:33 +1100

    module.exports

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 // Allows inquirer to be used on the page
 const inquirer = require('inquirer')
+const fs = require('fs')
 
 // Allows utilisation of the pages within the lib folder
-const employee = require('./lib/employee')
+//const employee = require('./lib/employee')
 const manager = require('./lib/manager')
 const engineer = require('./lib/engineer')
 const intern = require('./lib/intern')
 
+function companyCreation() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'companyName',
+            message: 'what is the name of your company?'
+        }
+    ])
+
+    .then(createManager())
+
+    .then(inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employeeGeneration',
+                message: 'What would you like to do next?',
+                choices: ['Create an engineer profile', new inquirer.Separator(), 'Create an intern profile', new inquirer.Separator(), 'Generate my HTML page', new inquirer.Separator()]
+            }
+        ]))
+}
+
+companyCreation()
