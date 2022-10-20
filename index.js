58b8cbd815cb8b3c18496ba0d398db12a19ab6d8
Miles Thomas <milesman2013@gmail.com>
2022-10-20 16:31:40 +1100

    massive reshuffle and reorder functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Allows inquirer to be used on the page
-const { default: inquirer } = require('inquirer')
+const inquirer = require('inquirer')
 const fs = require('fs')
 
 // Allows utilisation of the pages within the lib folder
@@ -8,49 +8,156 @@ const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 const HTMLFile = require('./dist/files')
-const { teamArray } = require('./lib/employee')
+
+const teamArray = []
 
 function companyCreation() {
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'companyName',
-            message: 'What is the name of your company?'
-        }
-    ])
+    function createManager() {
+        // The prompts used to create a manager
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the manager's name?"
+            },
+    
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the manager's employee ID?"
+            },
+    
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the manager's email address?"
+            },
+    
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is the employee's office number?"
+            }
+        ])
+        .then((answers) => {
+            const newManager = new Manager(`${answers.managerName}`, `${answers.id}`, `${answers.email}`, `${answers.officeNumber}`) 
+            Employee.teamArray.push(newManager)
+        })
+    } 
 
-    .then(createManager())
-
-    .then(inquirer
+    function createRoster() {
+        inquirer 
         .prompt([
             {
                 type: 'list',
-                name: 'employeeGeneration',
+                name: 'ProfileCreation',
                 message: 'What would you like to do next?',
-                choices: [
-                    { name: 'Create an engineer profile', value: 0 }, new inquirer.Separator(), 
-                    {name: 'Create an intern profile', value: 1}, new inquirer.Separator(), 
-                    {name: 'Generate my HTML page', value: 2}, new inquirer.Separator()
+                choices: ['Create an engineer', new inquirer.Separator(),
+                 'Create an intern', new inquirer.Separator, 
+                 'Create my webpage', new inquirer.Separator
                 ]
             }
-        ]))
-        .then((responses) => {
-            if (responses.employeeGeneration === 0) {
-                Engineer.createEngineer()
-            } else if (responses.employeeGeneration === 1) {
-                Intern.createIntern()
-            } else {
-                producePage()
+        ])
+        .then((answers) => {
+            switch (answers.ProfileCreation) {
+                case 'Create an engineer':
+                    createEngineer()
+                    break;
+                case 'Create an intern':
+                    createIntern()
+                    break;
+                case 'Create my webpage':
+                    producePage()
+                default: ''
+                    break;
             }
         })
-}
+    }
 
+    function createEngineer() {
+        // The prompt questions for creating a new engineer
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: "What is the engineer's name?"
+            },
+    
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the engineer's employee ID?"
+            },
+    
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the engineer's email address?"
+            },
+    
+            {
+                type: 'input',
+                name: 'github',
+                message: "Whats is the engineer's github ID?"
+            }
+        ])
+        .then((answers) => {
+            const newEngineer = new Engineer(`${answers.engineerName}`, `${answers.id}`, `${answers.email}`, `${answers.github}`) 
+            Employee.teamArray.push(newEngineer)
+        })
+        .then(() => createRoster())
+    }
+
+    function createIntern() {
+        // The prompts used to create a new intern
+        inquirer 
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "What is the intern's name?"
+            },
+    
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the intern's employee ID?"
+            },
+    
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the intern's email address?"
+            },
+            
+            {
+                type: 'input',
+                name: 'school',
+                message: "What school does the intern go to?"
+            }
+        ])
+    
+        .then((answers) => {
+            const newIntern = new Intern(`${answers.internName}`, `${answers.id}`, `${answers.email}`, `${answers.school}`) 
+            Employee.teamArray.push(newIntern)
+        })
+
+        .then(() => createRoster())
+    }
+
+    createManager()
+    .then(() => createRoster())
+
+}
 function producePage() {
+    // The function creating the new HTML page
     const fileTemplate = generateHTML()
     fs.writeFile('profile.html', fileTemplate, (err) => 
-    err ? console.error('README file creation has been unsuccessful') : console.log('README file has successfully been created'))
+    err ? console.error('HTML file creation has been unsuccessful') : console.log('Your HTML file has successfully been created'))
 
+    // Writing cards in the HTML
     for(let i = 0; i < Employee.teamArray.length; i++) {
         let employeeCard = document.createElement('div')
         employeeCard.classList.add('card col-4 bg-primary text-white')
