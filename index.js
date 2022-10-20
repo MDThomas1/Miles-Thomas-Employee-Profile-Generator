84d6b8988fe1d5de8a4a5a3cc4c2d8f0ac30b734
Miles Thomas <milesman2013@gmail.com>
2022-10-20 13:13:51 +1100

    added card generation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Allows inquirer to be used on the page
-const inquirer = require('inquirer')
+const { default: inquirer } = require('inquirer')
 const fs = require('fs')
 
 // Allows utilisation of the pages within the lib folder
@@ -8,6 +8,7 @@ const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 const HTMLFile = require('./dist/files')
+const { teamArray } = require('./lib/employee')
 
 function companyCreation() {
     inquirer
@@ -19,7 +20,7 @@ function companyCreation() {
         }
     ])
 
-    .then(Manager.createManager())
+    .then(createManager())
 
     .then(inquirer
         .prompt([
@@ -49,6 +50,34 @@ function producePage() {
     const fileTemplate = generateHTML()
     fs.writeFile('profile.html', fileTemplate, (err) => 
     err ? console.error('README file creation has been unsuccessful') : console.log('README file has successfully been created'))
+
+    for(let i = 0; i < Employee.teamArray.length; i++) {
+        let employeeCard = document.createElement('div')
+        employeeCard.classList.add('card col-4 bg-primary text-white')
+        let memberName = document.createElement('h4')
+        memberName.textContent = teamArray[i].employeeName
+        employeeCard.append(memberName)
+        let memberRole = document.createElement('h4')
+        memberRole.textContent = teamArray[i].role
+        employeeCard.append(memberRole)
+        let memberID = document.createElement('p')
+        memberID.textContent = 'ID: ' + teamArray[i].id
+        employeeCard.append(memberID)
+        let memberEmail = document.createElement('p')
+        memberEmail.textContent = 'Email: ' + teamArray[i].email
+        employeeCard.append(memberID)
+        let roleSpecific = document.createElement('p')
+        if (teamArray[i].officeNumber) {
+            roleSpecific.textContent = 'Office Number: ' + teamArray[i].officeNumber
+        } else if (teamArray[i].github) {
+            roleSpecific.textContent = 'Github: https://github.com/' + teamArray[i].github
+        } else if (teamArray[i].school) {
+            roleSpecific.textContent = 'School: ' + teamArray[i].school
+        }
+        employeeCard.append(roleSpecific)
+        document.querySelector('#employeeCards').append(employeeCard)
+        
+    }
 }
 
 companyCreation()
